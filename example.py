449ab0106a7c96d2284b7fcb7d6d1f865064e89f
jeremy.cote.08@gmail.com <jeremy.cote.08@gmail.com>
2023-06-20 16:11:15 -0400

    Formatted and tweaked a few functions

--- example.py
+++ example.py
@@ -5,6 +5,7 @@ Date created: 2023-06-16
 """
 
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
 import matplotlib
 import time
@@ -12,62 +13,70 @@ import networkx as nx
 from utilities import MSRO
 from vectorizedUtilities import vectorizedMSRO
 
-def getFirstOrdering(matrix):
-    """
-        - Purpose: Find the first 
-    """
-    first = []
-    for row in matrix:
-        first.append(np.nonzero(row)[0][0])
-    return first
-
-def getLastOrdering(matrix):
-    last = []
-    for row in matrix:
-        last.append(np.nonzero(row)[0][-1])
-    return last
 
 def getWidths(matrix):
     widths = []
     for row in matrix:
         elements = np.nonzero(row)[0]
         if len(elements) > 0:
-          width = np.max(elements) - np.min(elements)
+            width = np.max(elements) - np.min(elements)
         else:
-          width = 0
+            width = 0
         widths.append(width)
     return widths
 
+
 if __name__ == "__main__":
-    N = 300
-    M = 300
-    matrix = np.zeros((M,N), dtype = int)
+    seed = 111  # For reproducibility of the pseudodiameter
+    N = 50
+    M = 50
+    np.random.seed(seed=seed)
+    matrix = np.zeros((M, N), dtype=int)
     for i in range(M):
-        indices = np.random.choice(a = range(N), size = 3, replace = False)
+        indices = np.random.choice(a=range(N), size=3, replace=False)
         matrix[i, indices] = 1
-    
+
+    """
+    # Small test matrix
+    matrix = np.array(
+        [
+            [1, 0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 1, 0],
+            [1, 0, 1, 1, 0, 1],
+            [0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 0, 1],
+        ]
+    )"""
+
     W1, W2, W3 = 2, 1, 0.2
     algorithmTime = time.time()
-    #order = MSRO(input_matrix = matrix.T, W1 = W1, W2 = W2, W3 = W3)
-    order = vectorizedMSRO(input_matrix = matrix.T, W1 = W1, W2 = W2, W3 = W3)
+    orderSequential = MSRO(input_matrix=matrix.T, W1=W1, W2=W2, W3=W3, seed=seed)
+    order = vectorizedMSRO(input_matrix=matrix.T, W1=W1, W2=W2, W3=W3, seed=seed)
+    print("Order: ", order)
+    print("Seque: ", orderSequential)
+    print(
+        "Sequential and vectorized orderings the same? ",
+        np.allclose(order, orderSequential),
+    )
     finishTime = time.time()
     print("Total algorithm time: {}s".format(finishTime - algorithmTime))
 
     # Create a figure and post results
     # Original stats
     widths = getWidths(matrix)
-    
+
     # Reduced fill-in stats
     reordered = matrix.T[order].T
     reorderedWidths = getWidths(reordered)
-    
+
     print("Original matrix average width : ", np.mean(widths))
     print("Reordered matrix average width: ", np.mean(reorderedWidths))
 
-    fig, (ax1, ax2) = plt.subplots(1,2, figsize = (10, 8))
-    ax1.imshow(matrix, cmap = "binary")
-    ax2.imshow(reordered, cmap = "binary")
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 8))
+    ax1.imshow(matrix, cmap="binary")
+    ax2.imshow(reordered, cmap="binary")
     ax1.set_title(r"Original matrix ($W = {:.2f}$)".format(np.mean(widths)))
     ax2.set_title(r"Reduced matrix ($W = {:.2f}$)".format(np.mean(reorderedWidths)))
-    plt.savefig("reorderedMatrixN{}.pdf".format(N), dpi = 300)
-    #plt.show()
+    plt.savefig("reorderedMatrixN{}.pdf".format(N), dpi=300)
+    # plt.show()
--- vectorizedUtilities.py
+++ vectorizedUtilities.py
@@ -2,131 +2,217 @@
 Purpose: A set of efficient, matrix-based tools for calculating the reordering of a
          matrix's rows. As much as possible, I use matrix-based computations instead
          of loops.
+
+         The algorithm is iterative. At each step, it chooses the next index that is
+         part of the new ordering from a list of candidates. For efficiency, the
+         algorithm computes the function to score the candidates in parallel. The only
+         loop is the iterative while loop which builds the new order one-by-one.
+
+
 Date created: 2023-06-16
 """
 
 import numpy as np
 import networkx as nx
-from utilities import pseudodiameter, findActiveRows
+from utilities import pseudodiameter, find_active_rows
 from operator import itemgetter
 
-def change_in_frontsize_vectorized(front, rows, remainings):
-  """
-    - Purpose: Calculate the change in row and column frontsizes.
+
+def change_in_frontsize_vectorized(front, candidate_rows, remaining_rows):
+    """
+    - Purpose: Calculate the change in row and column frontsizes. The
+               data structure encodes the remaining rows of the matrix
+               for each choice of current row.
+
+               For example:
+
+               matrix =         [[0, 1, 1, 0, 0],
+                                 [1, 0, 0, 1, 0],
+                                 [1, 0, 0, 1, 1],
+                                 [1, 1, 1, 1, 0],
+                                 [0, 1, 0, 1, 0],
+                                 [1, 0, 1, 1, 1]]
+
+               front =          [[0, 1, 1, 0, 0],
+                                 [1, 0, 0, 1, 0]]
+
+               candidate_rows = [[1, 0, 0, 1, 1],
+                                 [1, 1, 1, 1, 0],
+                                 [0, 1, 0, 1, 0],
+                                 [1, 0, 1, 1, 1]]
+
+               remaining_rows = [
+                                 [[1, 1, 1, 1, 0],
+                                  [0, 1, 0, 1, 0],
+                                  [1, 0, 1, 1, 1]],
+
+                                 [[1, 0, 0, 1, 1],
+                                  [0, 1, 0, 1, 0],
+                                  [1, 0, 1, 1, 1]],
+
+                                 [[1, 0, 0, 1, 1],
+                                  [1, 1, 1, 1, 0],
+                                  [1, 0, 1, 1, 1]],
+
+                                 [[1, 0, 0, 1, 1],
+                                  [1, 1, 1, 1, 0],
+                                  [0, 1, 0, 1, 0]]
+                                ]
+
+                Note that each array in remaining_rows contains the remaining
+                rows in the matrix after we remove the front and the row that
+                corresponds to the same row in candidate_rows.
+
+                We can then use matrix multiplication to figure out which
+                columns are already part of the front and which ones are new,
+                as well as the columns which are now fully summed.
     - Input:
       - front (array): The front matrix before adding the current row.
-      - rows (array of arrays): The new rows to consider.
-      - remainings (array of arrays): The remaining rows.
+      - candidate_rows (array of arrays): The new rows to consider.
+      - remaining_rows (array of arrays): The remaining rows.
     - Output:
-      - (integer): The change in front sizes.
-  """
-  frontColumns = (~np.any(a = front, axis = 0)).astype(int)
-  newColumns = rows.astype(int) @ frontColumns.T
-  zerosInRemainings = (~np.any(a = remainings, axis = 1)).astype(int)
-  fullySummed = np.sum(a = zerosInRemainings @ rows.astype(int).T, axis = 1)
-  
-  return 1 + newColumns - 2 * fullySummed
+      - (integer): The change in front size.
+    """
+    front_columns = np.any(a=front, axis=0)
+    new_front_columns = candidate_rows.astype(int) @ (~front_columns).astype(int).T
+    zero_columns_remaining = (~np.any(a=remaining_rows, axis=1)).astype(int)
+    # print("Front columns shape: ", front_columns.shape)
+    # print("New front columns  : ", new_front_columns.shape)
+    # print("Zero columns shape : ", zero_columns_remaining.shape)
+    # print("Multiplication: ")
+    # print(zero_columns_remaining @ candidate_rows.astype(int).T)
+    # fully_summed = np.sum(
+    #    a=zero_columns_remaining @ candidate_rows.astype(int).T, axis=1
+    # )
+    # fully_summed = np.diag(
+    #    v=zero_columns_remaining @ candidate_rows.astype(int).T
+    # )
+    # Is this the fastest?
+    fully_summed = np.sum(a=zero_columns_remaining * candidate_rows, axis=1)
+    # print("Fully summed: ")
+    # print(fully_summed)
 
-def findFrontColumnsVectorized(matrix, rows):
-  """
-    - Purpose: For a given row, calculate the number of columns that
-               have nonzero elements and are already in the front.
+    return 1 + new_front_columns - 2 * fully_summed
+
+
+def find_front_columns_vectorized(front, candidate_rows):
+    """
+    - Purpose: For a given row, calculate nonzero indices that already
+               describe the nonzero columns in the front.
+
+               Note: I convert the Boolean matrices to integers because
+                     matrix multiplication reflects the logical operation
+                     I want to do.
     - Input:
-      - matrix (array): The current matrix that describes the front.
-      - rows (array of shape (number of matrices, len(matrix[0]))):
-               A multidimensional array holding all of the considered rows.
+      - front (array): The current matrix that describes the front.
+      - candidate_rows (array of arrays): A multidimensional array
+                                          holding the candidate rows.
     - Output:
-      - frontColumns (array of integers): The number of columns already in the front.
-  """
-  currentFrontColumns = (~np.any(a = matrix, axis = 0)).astype(int)
-  return rows.astype(int) @ currentFrontColumns.T
+      - front_columns (array of integers): The number of columns already in the front.
+    """
+    current_front_columns = np.any(a=front, axis=0)
+    return candidate_rows.astype(int) @ current_front_columns.astype(int).T
 
-def calculateOrderingVectorized(graph, matrix, W1 = 2, W2 = 1, W3 = 0.2, verbose = False):
-  """
+
+def calculate_ordering_vectorized(
+    graph, matrix, W1=2, W2=1, W3=0.2, seed=111, verbose=False
+):
+    """
     - Purpose: For a connected graph, calculate an ordering of the nodes.
     - Inputs:
       - graph (NetworkX object): The graph to reorder.
       - matrix (array): The matrix for the entire original graph.
       - W1, W2, W3 (positive integers): The weights for balancing the algorithm's
                                         considerations.
+      - seed (integer): For reproducibility of the pseudodiameter.
       - verbose (Boolean): If True, prints out information about the process.
     - Output:
       - order (list of integers): A permutation of the graph nodes.
-  """
-  rowGraph = matrix @ matrix.T
-  start, target = pseudodiameter(graph = graph)
-  distances = nx.algorithms.single_source_shortest_path_length(G = graph, source = target)
-  if verbose:
-    print("Distances: ", distances)
-  allNodes = graph.nodes()
-  order = [start]
-  while len(order) < len(graph):
-    # Find active rows
-    active = findActiveRows(reordered = order, rowGraph = rowGraph)
-    priorities = {}
-    front = matrix[order]
-    
-    # Build the matrix of indices for each list of
-    # ordered or unordered indices
-    multiple = np.tile(A = order, reps = (len(active), 1))
-    multiple = np.column_stack(tup = (multiple, active))
-    
-    unorderedIndices = np.setdiff1d(ar1 = allNodes, ar2 = order)
-    
-    # Still need to understand these sorting lines!
-    removeMask = (unorderedIndices==active[:,None]).any(0).nonzero()[0]
-    unorderedIndices = np.tile(A = unorderedIndices, reps = (len(active), 1))
-    m, n = unorderedIndices.shape
-    unorderedIndices = unorderedIndices[np.arange(n) != removeMask[:,None]].reshape(m,-1)
-    
-    # Build the rows and remaining matrices for each row of the orderedMask
-    rows = matrix[active]
-    remainings = matrix[unorderedIndices]
-    
-    # Calculate function values in matricized form
-    values = findFrontColumnsVectorized(matrix = front, rows = rows)
-    deltas = change_in_frontsize_vectorized(front = front, rows = rows, remainings = remainings)
-    score = -W1 * deltas + W2 * itemgetter(*tuple(active))(distances) - W3 * values
-    selectionIndex = np.argmax(a = score)
-    selection = active[selectionIndex]
-    order.append(selection)
+    """
+    row_graph = matrix @ matrix.T
+    start, target = pseudodiameter(graph=graph, seed=seed)
+    distances = nx.algorithms.single_source_shortest_path_length(G=graph, source=target)
     if verbose:
-      print("Start, target: ", start, target)
-      print("Active: ", active)
-      print("Priorities: ")
-      print(priorities)
-      print("Selection: ", selection)
-      print()
-  assert len(np.unique(order)) == len(allNodes)
-  #print("Ordering time: ", time.time() - orderTime)
-  return order
+        print("Distances: ", distances)
+    all_nodes = graph.nodes()
+    order = [start]
+    while len(order) < len(graph):
+        # Find active rows
+        active = find_active_rows(reordered=order, row_graph=row_graph)
+        front = matrix[order]
 
-def vectorizedMSRO(input_matrix, W1 = 2, W2 = 1, W3 = 0.2, verbose = False):
-  """
+        # Still need to understand these sorting lines!
+        # This deletes particular columns from each array so that
+        # we have only the unordered indices left
+        all_unordered_indices = np.setdiff1d(ar1=all_nodes, ar2=order)
+
+        # Find elements of each row to remove
+        removeMask = (all_unordered_indices == active[:, None]).any(axis=0).nonzero()[0]
+
+        # We will remove one element per row of this array
+        unordered_indices = np.tile(A=all_unordered_indices, reps=(len(active), 1))
+        # Keep all elements that are NOT part of the removeMask and then reshape
+        # From: https://stackoverflow.com/a/36502927
+        m, n = unordered_indices.shape
+        keep_mask = np.arange(n) != removeMask[:, None]
+        unordered_indices = unordered_indices[keep_mask].reshape(m, -1)
+
+        # Build the rows and remaining matrices for each row of the orderedMask
+        candidate_rows = matrix[active]
+        remaining_rows = matrix[unordered_indices]
+
+        # Calculate function values in matricized form
+        values = find_front_columns_vectorized(
+            front=front, candidate_rows=candidate_rows
+        )
+        deltas = change_in_frontsize_vectorized(
+            front=front, candidate_rows=candidate_rows, remaining_rows=remaining_rows
+        )
+        score = -W1 * deltas + W2 * itemgetter(*tuple(active))(distances) - W3 * values
+        selectionIndex = np.argmax(a=score)
+        selection = active[selectionIndex]
+        order.append(selection)
+        if verbose:
+            print("Start, target: ", start, target)
+            print("Active: ", active)
+            print("Selection: ", selection)
+            print()
+    assert len(np.unique(order)) == len(all_nodes)
+    return order
+
+
+def vectorizedMSRO(input_matrix, W1=2, W2=1, W3=0.2, seed=111, verbose=False):
+    """
     - Purpose: Reorder the rows of the input matrix to minimize its front.
     - Input:
       - input_matrix (array): The matrix to reorder.
       - W1, W2, W3 (positive integers): The weights for balancing the algorithm's
                                         considerations.
+      - seed (integer): For reproducibility of the pseudodiameter.
       - verbose (Boolean): If True, prints out information about the process.
     - Output:
-      - totalReordering (1D array): The permutation of the rows.
-  """
+      - total_reordering (1D array): The permutation of the rows.
+    """
 
-  # Only consider the nonzero pattern of elements
-  matrix = input_matrix.astype(bool)
-  
-  # Compute initial distances between rows of the matrix.
-  rowGraph = nx.from_numpy_array(matrix @ matrix.T)
+    # Only consider the nonzero pattern of elements
+    matrix = input_matrix.astype(bool)
 
-  # Check if the graph is disconnected and break into components
-  totalReordering = []
-  subgraphs = [rowGraph.subgraph(component).copy() for component in nx.connected_components(rowGraph)]
-  if verbose:
-    print("Component sizes: ", [len(i) for i in subgraphs])
-  for e, graph in enumerate(subgraphs):
+    # Compute initial distances between rows of the matrix.
+    row_graph = nx.from_numpy_array(matrix @ matrix.T)
+
+    # Check if the graph is disconnected and break into components
+    total_reordering = []
+    subgraphs = [
+        row_graph.subgraph(component).copy()
+        for component in nx.connected_components(row_graph)
+    ]
     if verbose:
-      print("Subgraph: ", e)
-    order = calculateOrderingVectorized(graph = graph, matrix = matrix, W1 = W1, W2 = W2, W3 = W3, verbose = verbose)
-    totalReordering.extend(order)
-  return totalReordering
+        print("Component sizes: ", [len(i) for i in subgraphs])
+    for e, graph in enumerate(subgraphs):
+        if verbose:
+            print("Subgraph: ", e)
+        order = calculate_ordering_vectorized(
+            graph=graph, matrix=matrix, W1=W1, W2=W2, W3=W3, seed=seed, verbose=verbose
+        )
+        total_reordering.extend(order)
+    return total_reordering
