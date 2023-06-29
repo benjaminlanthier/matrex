d89f0fdfcd740d5743020bf8d6baddebc785c0e9
benjaminlanthier <lanthier_benji@hotmail.com>
2023-06-29 10:29:59 -0400

    Initialized the `test_msro.py` file

---
+++ tests/test_msro.py
@@ -0,0 +1,37 @@
+"""
+File for testing the functions in `msro.py` to assure that it works as intended.
+"""
+
+import numpy as np
+
+from ..MatRexAlgs.msro import (
+    find_active_rows,
+    change_in_frontsize,
+    find_front_columns,
+    calculate_ordering,
+    msro
+)
+
+def test_find_active_rows():
+    # TODO
+    pass
+
+
+def test_chage_in_frontsize():
+    # TODO
+    pass
+
+
+def test_find_front_columns():
+    # TODO
+    pass
+
+
+def test_calculate_ordering():
+    # TODO
+    pass
+
+
+def test_msro():
+    # TODO
+    pass
