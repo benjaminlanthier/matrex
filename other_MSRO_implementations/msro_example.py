5f026e8511d5efc70e44064e012bed180a6c3a55
benjaminlanthier <lanthier_benji@hotmail.com>
2023-07-03 16:14:11 -0400

    Added official `msro()` function for comparison

--- other_MSRO_implementations/msro_example.py
+++ other_MSRO_implementations/msro_example.py
@@ -9,9 +9,15 @@ Date created: 2023-06-16
 
 # pylint: disable=C0103
 
+import sys, os
+
+sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
+
 import numpy as np
 from msro1 import msro as msro1
 from msro2 import msro as msro2
+
+from MatRexAlgs.msro import msro  # type: ignore
 
 
 # def get_widths(matrix):
@@ -30,8 +36,8 @@ if __name__ == "__main__":
     seed = 666  # For reproducibility of the pseudodiameter
     np.random.seed(seed=seed)
     nb_simulations = 100
-    min_size = 6
-    max_size = 6  # if max_size == min_size, only one matrix size will be generated per simulation
+    min_size = 20
+    max_size = 20  # if max_size == min_size, only one matrix size will be generated per simulation
     incrementation = 4
 
     weights = [2, 1, 0.2]
@@ -52,12 +58,13 @@ if __name__ == "__main__":
             #                    [0, 0, 0, 0, 0, 1]])
             msro1_order = msro1(input_matrix=matrix, W1=W1, W2=W2, W3=W3, seed=seed)
             msro2_order = msro2(matrix, perm="rows", seed=seed)
-            comparison.append(msro1_order == msro2_order)
+            msro_order = msro(input_matrix=matrix, weights=weights, seed=seed)
+            comparison.append([msro_order == msro1_order, msro_order == msro2_order])
             if count % 100 == 0:
                 print(f"msro1 order : {msro1_order}")
                 print(f"msro2 order : {msro2_order}")
                 print()
 
-        results = np.sum(comparison)
+        results = np.sum(comparison, axis=0)
         print(f"Number of permutations that were the same (size = {size}) : {results}")
         print(f"100% of the permutations are the same : {results == nb_simulations}")
