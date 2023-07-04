b2b15f74f94b66f091a6176e7545ea8baf344f6f
benjaminlanthier <lanthier_benji@hotmail.com>
2023-07-04 11:50:17 -0400

    Finished building the test functions for `msro()`

--- tests/test_msro.py
+++ tests/test_msro.py
@@ -19,27 +19,63 @@ from matrex.msro import (  # type: ignore
 
 def test_find_active_rows():
     """Test for the `matrex.msro.test_find_active_rows()` function."""
-    # TODO
-    pass
+    reordered = np.array([3, 1])
+    matrix = np.array(
+        [
+            [1, 0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 1, 0],
+            [1, 0, 1, 1, 0, 1],
+            [0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 0, 1],
+        ]
+    )
+    row_graph = matrix @ matrix.T
+    active_rows = find_active_rows(reordered, row_graph)
+    assert np.all(active_rows == np.array([0, 2, 4]))
 
 
 def test_change_in_frontsize():
     """Test for the `matrex.msro.change_in_frontsize()` function."""
-    # TODO
-    pass
+    # Test for this matrix with 2 rows in the front
+    # matrix = np.array([[0, 1, 1, 0, 0],
+    #                    [1, 0, 0, 1, 0],
+    #                    [1, 0, 0, 1, 1],
+    #                    [1, 1, 1, 1, 0],
+    #                    [0, 1, 0, 1, 0],
+    #                    [1, 0, 1, 1, 1]])
+    front = np.array([[0, 1, 1, 0, 0], [1, 0, 0, 1, 0]])
+    candidate_rows = np.array(
+        [[1, 0, 0, 1, 1], [1, 1, 1, 1, 0], [0, 1, 0, 1, 0], [1, 0, 1, 1, 1]]
+    )
+    remaining_rows = np.array(
+        [
+            [[1, 1, 1, 1, 0], [0, 1, 0, 1, 0], [1, 0, 1, 1, 1]],
+            [[1, 0, 0, 1, 1], [0, 1, 0, 1, 0], [1, 0, 1, 1, 1]],
+            [[1, 0, 0, 1, 1], [1, 1, 1, 1, 0], [1, 0, 1, 1, 1]],
+            [[1, 0, 0, 1, 1], [1, 1, 1, 1, 0], [0, 1, 0, 1, 0]],
+        ]
+    )
+    rcgain = change_in_frontsize(front, candidate_rows, remaining_rows)
+    assert np.all(rcgain == np.array([2, 1, 1, 2]))
 
 
 def test_find_front_columns():
     """Test for the `matrex.msro.find_front_columns()` function."""
     # TODO
-
-    pass
-
-
-def test_calculate_ordering():
-    """Test for the `matrex.msro.calculate_ordering()` function."""
-    # TODO
-    pass
+    # Test for this matrix with 2 rows in the front
+    # matrix = np.array([[0, 1, 1, 0, 0],
+    #                    [1, 0, 0, 1, 0],
+    #                    [1, 0, 0, 1, 1],
+    #                    [1, 1, 1, 1, 0],
+    #                    [0, 1, 0, 1, 0],
+    #                    [1, 0, 1, 1, 1]])
+    front = np.array([[0, 1, 1, 0, 0], [1, 0, 0, 1, 0]])
+    candidate_rows = np.array(
+        [[1, 0, 0, 1, 1], [1, 1, 1, 1, 0], [0, 1, 0, 1, 0], [1, 0, 1, 1, 1]]
+    )
+    front_columns = find_front_columns(front, candidate_rows)
+    assert np.all(front_columns == np.array([2, 4, 2, 3]))
 
 
 @pytest.mark.parametrize("m, n", [(10, 10), (20, 50), (50, 20), (100, 100)])
